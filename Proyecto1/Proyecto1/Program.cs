c4108063dda8943f8b35ed040f9d1920a6c975bf
rlope-rob <r.lopez07@outlook.es>
2024-11-18 14:53:29 -0500

    configurando API

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+
 
 namespace Proyecto1
 {
     internal static class Program
     {
+        // Variable global para acceder a la configuración
+        public static IConfiguration Configuration;
+
         /// <summary>
-        /// The main entry point for the application.
+        /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            // Configurar el cargador de appsettings.json
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            // Construir la configuración
+            Configuration = builder.Build();
+
+            // Iniciar la aplicación
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmCalculadora());
